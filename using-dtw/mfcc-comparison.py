bc81ce661ae161382c1baa34f5449a44f9b3f609
Owais <oowais@gmail.com>
2018-12-21 15:01:07 +0100

    update plots

--- using-dtw/mfcc-comparison.py
+++ using-dtw/mfcc-comparison.py
@@ -2,31 +2,44 @@ import librosa
 import matplotlib.pyplot as plt
 from dtw import dtw
 
-
 if __name__ == '__main__':
-
     # Loading audio files
-    y1, sr1 = librosa.load('../audio-resources/blues0.mp3')
-    y2, sr2 = librosa.load('../audio-resources/blues1.mp3')
+    file1 = 'blues0.mp3'
+    file2 = 'rock1.mp3'
 
-    # Showing multiple plots using subplot
-    plt.subplot(1, 2, 1)
-    mfcc1 = librosa.feature.mfcc(y1, sr1)   # Computing MFCC values
-    # librosa.display.specshow(mfcc1)
+    y1, sr1 = librosa.load('../audio-resources/' + file1)
+    y2, sr2 = librosa.load('../audio-resources/' + file2)
 
-    plt.subplot(1, 2, 2)
+    mfcc1 = librosa.feature.mfcc(y1, sr1)
     mfcc2 = librosa.feature.mfcc(y2, sr2)
-    # librosa.display.specshow(mfcc2)
 
-    from sklearn.metrics.pairwise import euclidean_distances, manhattan_distances
-    from nltk.metrics.distance import edit_distance
+    # Showing multiple plots in same figure using subplot
+    plt.subplot(1, 2, 1)
+    plt.title('MFCC of ' + file1)
+    plt.imshow(mfcc1, aspect='auto', origin='lower')
+    plt.subplot(1, 2, 2)
+    plt.title('MFCC of ' + file2)
+    plt.imshow(mfcc2, aspect='auto', origin='lower')
 
-    dist_fun = euclidean_distances
+    # Showing plots in separate figures
+    plt.figure(num='MFCC of ' + file1)
+    plt.title('MFCC of ' + file1)
+    plt.imshow(mfcc1, aspect='auto', origin='lower')
 
-    dist, cost, path = dtw(mfcc1.T, mfcc2.T, dist_fun)
-    print("The normalized distance between the two : ",dist)   # 0 for similar audios
+    plt.figure(num='MFCC of ' + file2)
+    plt.title('MFCC of ' + file2)
+    plt.imshow(mfcc2, aspect='auto', origin='lower')
 
-    plt.imshow(cost.T, origin='lower', cmap=plt.get_cmap('gray'), interpolation='nearest')
-    plt.plot(path[0], path[1], 'w')   # creating plot for DTW
-
-    plt.show()  # To display the plots graphically
+    # from sklearn.metrics.pairwise import euclidean_distances, manhattan_distances
+    # from nltk.metrics.distance import edit_distance
+    #
+    # dist_fun = manhattan_distances
+    #
+    # dist, cost, path = dtw.dtw(mfcc1.T, mfcc2.T, dist_fun)
+    # print("The normalized distance between the two : ", dist)  # 0 for similar audios
+    #
+    # plt.figure()
+    # plt.imshow(cost.T, origin='lower', cmap=plt.get_cmap('gray'), interpolation='nearest')
+    # plt.plot(path[0], path[1], 'w')  # creating plot for DTW
+    #
+    # plt.show()  # To display the plots graphically
