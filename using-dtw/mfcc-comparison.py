0ec80a5b96b366120efb6ec6adee45060e62f3e8
Owais <oowais@gmail.com>
2018-12-21 15:28:13 +0100

    changed euclidean method used to find closeness

--- using-dtw/mfcc-comparison.py
+++ using-dtw/mfcc-comparison.py
@@ -1,6 +1,7 @@
 import librosa
 import matplotlib.pyplot as plt
 from dtw import dtw
+from scipy.spatial.distance import euclidean
 
 if __name__ == '__main__':
     # Loading audio files
@@ -10,8 +11,8 @@ if __name__ == '__main__':
     y1, sr1 = librosa.load('../audio-resources/' + file1)
     y2, sr2 = librosa.load('../audio-resources/' + file2)
 
-    mfcc1 = librosa.feature.mfcc(y1, sr1)
-    mfcc2 = librosa.feature.mfcc(y2, sr2)
+    mfcc1 = librosa.feature.mfcc(y1, sr1, n_mfcc=2)
+    mfcc2 = librosa.feature.mfcc(y2, sr2, n_mfcc=2)
 
     # Showing multiple plots in same figure using subplot
     plt.subplot(1, 2, 1)
@@ -30,16 +31,13 @@ if __name__ == '__main__':
     plt.title('MFCC of ' + file2)
     plt.imshow(mfcc2, aspect='auto', origin='lower')
 
-    # from sklearn.metrics.pairwise import euclidean_distances, manhattan_distances
-    # from nltk.metrics.distance import edit_distance
-    #
-    # dist_fun = manhattan_distances
-    #
-    # dist, cost, path = dtw.dtw(mfcc1.T, mfcc2.T, dist_fun)
-    # print("The normalized distance between the two : ", dist)  # 0 for similar audios
-    #
-    # plt.figure()
-    # plt.imshow(cost.T, origin='lower', cmap=plt.get_cmap('gray'), interpolation='nearest')
-    # plt.plot(path[0], path[1], 'w')  # creating plot for DTW
-    #
-    # plt.show()  # To display the plots graphically
+    dist_func = euclidean
+
+    dist, cost, path = dtw.dtw(mfcc1.T, mfcc2.T, dist_func)
+    print("The normalized distance between the two : ", dist)  # 0 for similar audios
+
+    plt.figure()
+    plt.imshow(cost.T, origin='lower', cmap=plt.get_cmap('gray'), interpolation='nearest')
+    plt.plot(path[0], path[1], 'w')  # creating plot for DTW
+
+    plt.show()  # To display the plots graphically
