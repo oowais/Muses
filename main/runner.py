3747f9b0abb31a1c989d66962ce884c5e45207f1
Owais <oowais@gmail.com>
2019-01-03 15:46:21 +0100

    Update runner.py

--- main/runner.py
+++ main/runner.py
@@ -1,204 +1,117 @@
-from main.db import Db
-from main.datastructure.feature import Feature
-from main.datastructure.distance import Distance
-from main.extractor import Extractor
-from main.hash import sha256sum
-from main.scale import scale
 import os
+import time
+
+from db import Db
+from extractor import Extractor
+from util import sha256sum, scale, get_name
 
 database_name = 'db.sqlite'
 audio_folder_name = 'audio_resources'
-root_dir_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+root_dir_path = os.path.abspath(os.path.dirname(__file__))
 db_file = os.path.join(root_dir_path, database_name)
 audio_path = os.path.join(root_dir_path, audio_folder_name)
 
 
-def initialize_extreme_dist_table_and_update_it():
-    db = Db(storage_file=db_file)
-    min_feature = Feature(mfcc=0.0, chroma_cens=0.0, chroma_stft=0.0, mel=0.0, tonnetz=0.0, rhythm=0.0)
-    max_feature = Feature(mfcc=0.0, chroma_cens=0.0, chroma_stft=0.0, mel=0.0, tonnetz=0.0, rhythm=0.0)
-    db.set_initial_extreme_distance(min_feature=min_feature, max_feature=max_feature)
-    db.update_extreme_distance('rhythm', 3.53334, 22.5226)
-
-
-def get_features_and_save_to_db_test():
-    name1 = 'metal0.mp3'
-    name2 = 'metal1.mp3'
-    file1 = '../audio_resources/' + name1
-    file2 = '../audio_resources/' + name2
-
-    ex = Extractor()
-
-    feature1 = ex.get_all_features(file1)
-    feature2 = ex.get_all_features(file2)
-
-    dist = ex.get_distance(feature1, feature2)
-
-    db = Db(storage_file=db_file)
-    # print('deleting tables: ', db.delete_tables())
-    print('creating tables: ', db.create_tables())
-    print('Saving into db: ', db.save_feature_distances(dist_obj=dist))
-
-
-def test_get_hash_from_db():
-    db = Db(storage_file=db_file)
-    hash1 = '9e0035d3ef4d0f06bfa161f328e6bb78a5e20e291b46463722dddf2eee62b1ab'
-    hash2 = 'e4d5bb5509746f7ed8feb98ea41fa797a30ebf6dda68507b094bb4d3b4ae7c89'
-    dist = db.get_distance(hash2, hash1)
-    print(dist)
-
-
-def test_update_factor():
-    db = Db(storage_file=db_file)
-    hash2 = '9e0035d3ef4d0f06bfa161f328e6bb78a5e20e291b46463722dddf2eee62b1ab'
-    hash1 = 'e4d5bb5509746f7ed8feb98ea41fa797a30ebf6dda68507b094bb4d3b4ae7c89'
-    dist = Distance(hash1=hash1, hash2=hash2, mfcc_dist=1.1, chroma_cens_dist=1.1, chroma_stft_dist=1.0, mel_dist=1.0,
-                    tonnetz_dist=1.0, rhythm_dist=1.0)
-    print(db.update_factors(dist))
-
-
-def check_hashes_present():
-    db = Db(storage_file=db_file)
-    hash1 = '9e0035d3ef4d0f06bfa161f328e6bb78a5e20e291b46463722dddf2eee62b1ab'
-    hash2 = 'e4d5bb5509746f7ed8feb98ea41fa797a30ebf6dda68507b094bb4d3b4ae7c89'
-    print(db.is_hashes_present(hash1, hash2))
-
-
-def get_extreme_distance():
-    db = Db(storage_file=db_file)
-    asd = db.get_extreme_distances()
-    print(asd)
-
-
-def init_extreme_table(db):
-    max = 1.7976931348623157e+308
-    min_feature = Feature(mfcc=max, chroma_cens=max, chroma_stft=max, mel=max, tonnetz=max, rhythm=max)
-    max_feature = Feature(mfcc=0.0, chroma_cens=0.0, chroma_stft=0.0, mel=0.0, tonnetz=0.0, rhythm=0.0)
-    return db.set_initial_extreme_distance(min_feature=min_feature, max_feature=max_feature)
-
-
-def get_features():
+def get_features_and_distance():
+    """
+    Loop through the audio_resources folder and caluclate features of each track and the distance between them
+    """
+    start_time = time.time()
     ex = Extractor()
     db = Db(storage_file=db_file)
     print('Creating tables if not present...')
     if not db.create_tables():
         return
-    print('Initializing extreme distance tables...')
-    if not init_extreme_table(db):
-        return
 
     # todo: loop over the directory
-    list_of_files = os.listdir(audio_path)
-    list_of_files_processed = []
+    file_list = os.listdir(audio_path)
+    file_list_size = len(file_list)
+    processed_file_list = []
     i = 0
-    while i < len(list_of_files):
-        ifile = list_of_files[i]
-        # todo: get two audios
-        if ifile not in list_of_files_processed:
-            list_of_files_processed.append(ifile)
-            ifeature = ex.get_all_features(os.path.join(audio_path, ifile))
+    while i < file_list_size:
+        ifile = file_list[i]
+        # get two audios
+        if ifile not in processed_file_list:
+            update_ifile = 1
+            processed_file_list.append(ifile)
+            print('------------------------')
+            # using names instead of hashes so that while checking results its easier to read
+            ifile_sha = sha256sum(os.path.join(audio_path, ifile))
             j = i + 1
-            while j < len(list_of_files):
-                jfile = list_of_files[j]
-
-                ifile_sha = sha256sum(os.path.join(audio_path, ifile))
+            while j < file_list_size:
+                jfile = file_list[j]
                 jfile_sha = sha256sum(os.path.join(audio_path, jfile))
-                # todo: check if their hash is already present in the distance table
+
+                # check if their hash is already present in the distance table
                 if db.is_hashes_present(ifile_sha, jfile_sha):
                     print(ifile, ' and ', jfile, ' already present in database')
                     j += 1
                     continue
                 else:
-                    # todo: if not, calculate features and distances and save to db
+                    # if not, calculate features and distances and save to db
+                    if update_ifile:
+                        ifeature = ex.get_all_features(os.path.join(audio_path, ifile))
+                        update_ifile = 0
                     jfeature = ex.get_all_features(os.path.join(audio_path, jfile))
                     dist = ex.get_distance(ifeature, jfeature)
                     db.save_feature_distances(dist)
-
-                    # should compute its factors here but will do for all together in the end
-
-                    # todo: check if the distances of features is the min or max
-                    ext_dist = db.get_extreme_distances()
-                    # todo: if true, then update the extreme_distance_table
-                    if dist.mfcc_dist > ext_dist[0][2]:
-                        db.update_max_distance('mfcc', dist.mfcc_dist)
-                    if dist.mfcc_dist < ext_dist[0][1]:
-                        db.update_min_distance('mfcc', dist.mfcc_dist)
-
-                    if dist.chroma_cens_dist > ext_dist[1][2]:
-                        db.update_max_distance('chroma_cens', dist.chroma_cens_dist)
-                    if dist.chroma_cens_dist < ext_dist[1][1]:
-                        db.update_min_distance('chroma_cens', dist.chroma_cens_dist)
-
-                    if dist.chroma_stft_dist > ext_dist[2][2]:
-                        db.update_max_distance('chroma_stft', dist.chroma_stft_dist)
-                    if dist.chroma_stft_dist < ext_dist[2][1]:
-                        db.update_min_distance('chroma_stft', dist.chroma_stft_dist)
-
-                    if dist.mel_dist > ext_dist[3][2]:
-                        db.update_max_distance('mel', dist.mel_dist)
-                    if dist.mel_dist < ext_dist[3][1]:
-                        db.update_min_distance('mel', dist.mel_dist)
-
-                    if dist.tonnetz_dist > ext_dist[4][2]:
-                        db.update_max_distance('tonnetz', dist.tonnetz_dist)
-                    if dist.tonnetz_dist < ext_dist[4][1]:
-                        db.update_min_distance('tonnetz', dist.tonnetz_dist)
-
-                    if dist.rhythm_dist > ext_dist[5][2]:
-                        db.update_max_distance('rhythm', dist.rhythm_dist)
-                    if dist.rhythm_dist < ext_dist[5][1]:
-                        db.update_min_distance('rhythm', dist.rhythm_dist)
                 j += 1
         i += 1
-
-
-def calculate_factors():
-    print('Calcuating factors....')
-    db = Db(storage_file=db_file)
-    distances = db.get_complete_distance_table()
-    extremes = db.get_extreme_distances()
-    for dist in distances:
-        # check if previously its value is 0.0, here only checking if mfcc = 0
-        if dist[3] == 0.0:
-            # todo: calculate the new factor value based on the new extreme values
-            mfcc_factor = scale(rmin=extremes[0][1], rmax=extremes[0][2], val=dist[2])
-            chroma_cens_factor = scale(rmin=extremes[1][1], rmax=extremes[1][2], val=dist[4])
-            chroma_stft_factor = scale(rmin=extremes[2][1], rmax=extremes[2][2], val=dist[6])
-            mel_factor = scale(rmin=extremes[3][1], rmax=extremes[3][2], val=dist[8])
-            tonnetz_factor = scale(rmin=extremes[4][1], rmax=extremes[4][2], val=dist[10])
-            rhythm_factor = scale(rmin=extremes[5][1], rmax=extremes[5][2], val=dist[12])
-            new_dist_factor = Distance(hash1=dist[0], hash2=dist[1], mfcc_dist=mfcc_factor,
-                                       chroma_cens_dist=chroma_cens_factor, chroma_stft_dist=chroma_stft_factor,
-                                       mel_dist=mel_factor, tonnetz_dist=tonnetz_factor, rhythm_dist=rhythm_factor)
-            # todo: and update that feature value in all the rows in feature table
-            db.update_factors(new_dist_factor)
+    print("Done! Took %.0f seconds to calculate features and distances between " % (
+            time.time() - start_time) + str(file_list_size) + " files")
 
 
 def print_factors():
     db = Db(storage_file=db_file)
-    factors = db.get_all_factors()
-    t = 0
-    min = 0
-    max = 0
-    list = []
-    for fact in factors:
-        sum = fact[2] + fact[3] + fact[4] + fact[5] + fact[6] + fact[7]
-        if t == 0:
-            min = sum
-            max = sum
-            t += 1
-        else:
-            if sum < min:
-                min = sum
-            elif sum > max:
-                max = sum
+    file_list = os.listdir(audio_path)
+    name_list = []
 
-    for fact in factors:
-        sum = fact[2] + fact[3] + fact[4] + fact[5] + fact[6] + fact[7]
-        print(fact[0], ' and ', fact[1], ' ==> ', scale(min, max, sum))
+    for file in file_list:
+        name_list.append(get_name(file))
+    factors = db.get_all_distances()
+    while True:
+        print()
+        var = 0
+        while var < len(name_list):
+            print(var + 1, ' ', name_list[var])
+            var += 1
+        val = int(input('Select a track number(0 to exit)...'))
+        if val == 0:
+            break
+        elif val > len(name_list):
+            print('Enter a valid number, Press enter to continue')
+            input()
+            continue
+        print()
+        selected_track = name_list[val - 1]
+
+        sum_list = []
+        for fact in factors:
+            # sum_list.append(fact[2] + fact[3] + fact[4] + fact[5] + fact[6] + fact[7])
+            # not using chroma_cens and mel feature
+            sum_list.append(fact[2] + fact[4] + fact[6] + fact[7])
+
+        min_val = min(sum_list)
+        max_val = max(sum_list)
+
+        print('Tracks closest to ', selected_track, '(in ascending order):')
+        result = []
+        i = 0
+        while i < len(factors):
+            scaled_sum = scale(rmin=min_val, rmax=max_val, val=sum_list[i])
+            if factors[i][0] == selected_track:
+                result.append((factors[i][1], scaled_sum))
+            elif factors[i][1] == selected_track:
+                result.append((factors[i][0], scaled_sum))
+            i += 1
+
+        result.sort(key=lambda tup: tup[1])
+        for a in result:
+            print(a[0])
+        print('---------------------------------------------------------------')
+        print()
 
 
 if __name__ == '__main__':
-    get_features()
-    calculate_factors()
+    get_features_and_distance()
     print_factors()
