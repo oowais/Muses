aa573d15438021ffeb71615115368a8289c7f1b2
Owais <oowais@gmail.com>
2019-01-03 15:45:00 +0100

    Update extractor.py

--- main/extractor.py
+++ main/extractor.py
@@ -2,9 +2,11 @@ import logging
 import librosa
 import fastdtw
 from scipy.spatial.distance import euclidean
-from main.datastructure.feature import Feature
-from main.datastructure.distance import Distance
-from main.hash import sha256sum
+
+from datastructure.feature import Feature
+from datastructure.distance import Distance
+from util import sha256sum
+from util import get_name
 
 
 class Extractor:
@@ -13,9 +15,9 @@ class Extractor:
         self.logger = logging.getLogger(__name__)
 
     def get_all_features(self, file):
-        file_list = file.split('/')
-        name = file_list[len(file_list) - 1]
-        print('Processing ', name, '...')
+        name = get_name(file)
+        self.logger.info('Processing ', name)
+        print('Processing ', name)
         y, sr = librosa.load(file)
 
         # Calculating mfcc feature
@@ -28,9 +30,7 @@ class Extractor:
         # Rhythm
         rhythm = librosa.feature.tempogram(y=y, sr=sr)
 
-        # Currently using name instead of hash value
-        # hash = sha256sum(file)
-        hash = name
+        hash = sha256sum(file)
 
         return Feature(hash=hash, name=name, mfcc=mfcc, chroma_cens=chroma_cens, chroma_stft=chroma_stft, mel=mel,
                        tonnetz=tonnetz, rhythm=rhythm)
@@ -40,13 +40,13 @@ class Extractor:
         print(feature1.name + ' <==> ' + feature2.name)
         dist_func = euclidean
 
-        distance1, new_path = fastdtw.fastdtw(feature1.mfcc.T, feature2.mfcc.T, dist=dist_func)
-        distance2, new_path2 = fastdtw.fastdtw(feature1.chroma_cens.T, feature2.chroma_cens.T, dist=dist_func)
-        distance3, new_path3 = fastdtw.fastdtw(feature1.chroma_stft.T, feature2.chroma_stft.T, dist=dist_func)
-        distance4, new_path4 = fastdtw.fastdtw(feature1.mel.T, feature2.mel.T, dist=dist_func)
-        distance5, new_path5 = fastdtw.fastdtw(feature1.tonnetz.T, feature2.tonnetz.T, dist=dist_func)
-        distance6, new_path6 = fastdtw.fastdtw(feature1.rhythm.T, feature2.rhythm.T, dist=dist_func)
+        distance1, path1 = fastdtw.fastdtw(feature1.mfcc.T, feature2.mfcc.T, dist=dist_func)
+        distance2, path2 = fastdtw.fastdtw(feature1.chroma_cens.T, feature2.chroma_cens.T, dist=dist_func)
+        distance3, path3 = fastdtw.fastdtw(feature1.chroma_stft.T, feature2.chroma_stft.T, dist=dist_func)
+        distance4, path4 = fastdtw.fastdtw(feature1.mel.T, feature2.mel.T, dist=dist_func)
+        distance5, path5 = fastdtw.fastdtw(feature1.tonnetz.T, feature2.tonnetz.T, dist=dist_func)
+        distance6, path6 = fastdtw.fastdtw(feature1.rhythm.T, feature2.rhythm.T, dist=dist_func)
 
-        return Distance(hash1=feature1.hash, hash2=feature2.hash, mfcc_dist=distance1, chroma_cens_dist=distance2,
-                        chroma_stft_dist=distance3, mel_dist=distance4, tonnetz_dist=distance5,
-                        rhythm_dist=distance6)
+        return Distance(hash1=feature1.hash, hash2=feature2.hash, name1=feature1.name, name2=feature2.name,
+                        mfcc_dist=distance1, chroma_cens_dist=distance2, chroma_stft_dist=distance3,
+                        mel_dist=distance4, tonnetz_dist=distance5, rhythm_dist=distance6)
