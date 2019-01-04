d2fe85a53f17d19bdf8d71b539b680078cf91975
Owais <oowais@gmail.com>
2019-01-04 12:41:37 +0100

    Added progress bar while processing audio files

--- runner.py
+++ runner.py
@@ -1,10 +1,10 @@
 import os
 import time
-from threading import Thread
 
 from db import Db
-from extractor import Extractor
-from util import sha256sum, scale, get_name
+from util import sha256sum, scale, get_name, progress, sum_n
+from thread import ThreadWithReturnValue
+from extractor import get_all_features, get_distance
 
 database_name = 'db.sqlite'
 audio_folder_name = 'audio_resources'
@@ -13,46 +13,33 @@ db_file = os.path.join(root_dir_path, database_name)
 audio_path = os.path.join(root_dir_path, audio_folder_name)
 
 
-class ThreadWithReturnValue(Thread):
-    def __init__(self, group=None, target=None, name=None,
-                 args=(), kwargs={}, Verbose=None):
-        Thread.__init__(self, group, target, name, args, kwargs)
-        self._return = None
-
-    def run(self):
-        if self._target is not None:
-            self._return = self._target(*self._args, **self._kwargs)
-
-    def join(self, *args):
-        Thread.join(self, *args)
-        return self._return
-
-
 def get_features_and_distance():
     """
-    Loop through the audio_resources folder and caluclate features of each track and the distance between them
+    Loop through the audio_resources folder and calculate features of each track and the distance between them
     """
     start_time = time.time()
-    ex = Extractor()
     db = Db(storage_file=db_file)
-    print('Creating tables if not present...')
     if not db.create_tables():
+        print('Problems in creating tables')
         return
 
-    # todo: loop over the directory
+    # loop over the directory
     file_list = os.listdir(audio_path)
     file_list_size = len(file_list)
     processed_file_list = []
     i = 0
+    processing = False
+    total_prog = sum_n(file_list_size + 1)
+    curr_prog = 0
+    print('Processing...')
     while i < file_list_size:
         ifile = file_list[i]
         # get two audios
         if ifile not in processed_file_list:
             ipath = os.path.join(audio_path, ifile)
-            update_ifile = 1
+            update_ifile = True
+
             processed_file_list.append(ifile)
-            print('------------------------')
-            # using names instead of hashes so that while checking results its easier to read
             ifile_sha = sha256sum(os.path.join(audio_path, ifile))
             j = i + 1
             while j < file_list_size:
@@ -62,38 +49,50 @@ def get_features_and_distance():
 
                 # check if their hash is already present in the distance table
                 if db.is_hashes_present(ifile_sha, jfile_sha):
-                    print(ifile, ' and ', jfile, ' already present in database')
                     j += 1
                     continue
                 else:
                     # if not, calculate features and distances and save to db
                     if update_ifile:
-                        ithread = ThreadWithReturnValue(target=ex.get_all_features, args=(ipath,))
+                        ithread = ThreadWithReturnValue(target=get_all_features, args=(ipath,))
                         ithread.start()
                         # ifeature = ex.get_all_features(os.path.join(audio_path, ifile))
-                        update_ifile = 0
-                    jthread = ThreadWithReturnValue(target=ex.get_all_features, args=(jpath,))
+                    jthread = ThreadWithReturnValue(target=get_all_features, args=(jpath,))
                     jthread.start()
-                    ifeature = ithread.join()
+
+                    if update_ifile:
+                        ifeature = ithread.join()
+                        update_ifile = False
+                        curr_prog += 1
+                        progress(scale(0, total_prog, curr_prog, 0, 1))
                     jfeature = jthread.join()
                     # jfeature = ex.get_all_features(os.path.join(audio_path, jfile))
 
                     # dist = ex.get_distance(ifeature, jfeature)
                     # db.save_feature_distances(dist)
-                    if i > 0:
+                    if processing:  # distances can be calculated until the next features are calculated
                         dist_thread.join()
-                    dist_thread = ThreadWithReturnValue(target=save_feature, args=(db, ex, ifeature, jfeature,))
+                    dist_thread = ThreadWithReturnValue(target=save_feature, args=(db, ifeature, jfeature,))
                     dist_thread.start()
+                    curr_prog += 1
+                    progress(scale(0, total_prog, curr_prog, 0, 1))
+                    processing = True
                 j += 1
         i += 1
-    # todo: check if its an object or not
-    dist_thread.join()
-    print("Done! Took %.0f seconds to calculate features and distances between " % (
-            time.time() - start_time) + str(file_list_size) + " files")
+    if processing:
+        dist_thread.join()
+        message = "Done! Took %.0f seconds to calculate features and distances between " % (
+                time.time() - start_time) + str(file_list_size) + " files"
+    else:
+        message = 'Data collected from database'
+
+    progress(1)
+    print()
+    print(message)
 
 
-def save_feature(db, ex, ifeature, jfeature):
-    dist = ex.get_distance(ifeature, jfeature)
+def save_feature(db, ifeature, jfeature):
+    dist = get_distance(ifeature, jfeature)
     db.save_feature_distances(dist)
 
 
@@ -108,10 +107,19 @@ def print_factors():
     while True:
         print()
         var = 0
+        # Printing name of files from the folder
         while var < len(name_list):
-            print(var + 1, ' ', name_list[var])
-            var += 1
-        val = int(input('Select a track number(0 to exit)...'))
+            print('%-30s %-30s' % (str(var + 1) + ' ' + name_list[var], str(var + 2) + ' ' + name_list[var + 1]))
+            var += 2
+
+        # Getting a file number
+        try:
+            val = int(input('Select a track number(0 to exit)...'))
+        except ValueError as e:
+            print('Enter a valid number, Press enter to continue')
+            input()
+            continue
+
         if val == 0:
             break
         elif val > len(name_list):
@@ -123,8 +131,8 @@ def print_factors():
 
         sum_list = []
         for fact in factors:
-            # sum_list.append(fact[2] + fact[3] + fact[4] + fact[5] + fact[6] + fact[7])
             # not using chroma_cens and mel feature
+            # sum_list.append(fact[2] + fact[3] + fact[4] + fact[5] + fact[6] + fact[7])
             sum_list.append(fact[2] + fact[4] + fact[6] + fact[7])
 
         min_val = min(sum_list)
@@ -141,10 +149,13 @@ def print_factors():
                 result.append((factors[i][0], scaled_sum))
             i += 1
 
+        # Sorting according to distance
         result.sort(key=lambda tup: tup[1])
         for a in result:
             print(a[0])
         print('---------------------------------------------------------------')
+        print('Press Enter to continue...')
+        input()
         print()
 
 
