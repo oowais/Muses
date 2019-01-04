acfda59d6ed113aa0e573562f2f447641b8fc512
Owais <oowais@gmail.com>
2019-01-04 14:25:47 +0100

    Update thread.py

--- thread.py
+++ thread.py
@@ -11,6 +11,6 @@ class ThreadWithReturnValue(Thread):
         if self._target is not None:
             self._return = self._target(*self._args, **self._kwargs)
 
-    def join(self, *args    ):
+    def join(self, *args):
         Thread.join(self, *args)
         return self._return
